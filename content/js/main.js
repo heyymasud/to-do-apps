9030517192bf3a89c37290955023fdcee421b692
heyymasud <heyymasud@gmail.com>
2024-06-18 10:19:20 +0700

    first commit

---
+++ content/js/main.js
@@ -0,0 +1,23 @@
+// VARIABEL
+const addTask = document.getElementById('addtask');
+
+
+// Tambah Task
+const inputField = function () {
+    
+}
+
+
+
+/* 
+Detail komponen :
+Profile
+waktu
+text area
+level prioritas
+button submit
+kolom centang
+to do
+done
+delete
+*/
